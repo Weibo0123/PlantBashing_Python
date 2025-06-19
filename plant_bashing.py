834733f2d948cf33fbd0001544aafcecd62b71e6
Weibo0123 <weibo322@gamil.com>
2025-06-19 09:59:30 -0500

    done all the functions

--- plant_bashing.py
+++ plant_bashing.py
@@ -1,22 +1,29 @@
 import time
 import random
 import sys
+import os
 #print("is running at START")
 name = ""
 day = 1
 height = 0
 leaves = 0
 play_time = 0
+weather = ["Rainy", "Sunny", "Cloudy", "Overcast", "Windstorm", "Rainy", "Foggy"]
+plantNames= ["Morpheus", "Analies", "Izzy"]
+morpheus = 0
+analies=0
+izzy = 0
+growth_rate = 0
 
 
 def greet_player():
+    os.system('clear')
     global name
     print("Welcome to the game!")
     name = input("What is your name? ")
     print(f"Hello {name}!")
     time.sleep(3)
-    assign_name()
-    print("In the Computer's digital world, time moves much faster than it does outside.")
+    
 
 
 def if_answer_valid(input):
@@ -24,11 +31,42 @@ def if_answer_valid(input):
     return input in ['y', 'yes', 'n', 'no']
 
 
-def choose_weathear():
-    weather = ["Rainy", "Sunny", "Cloudy", "Overcast", "Windstorm", "Rainy", "Foggy"]
+def choose_weather():
+    global weather
     picked =random.choice(weather)
 
     return picked
+
+def weather_effects(weather):
+    global growth_rate
+    global leaves
+
+    if weather == "Rainy":
+        growth = False
+        growth_rate += 2
+    elif weather == "Sunny":
+        growth = True
+        growth_rate += 3
+    elif weather == "Cloudy":
+        growth = False
+    elif weather == "Overcast":
+        growth = True
+    elif weather == "Windstorm":
+        growth = False
+        growth_rate -= 2
+        leaves -= 2
+    elif weather == "Foggy":
+        growth = False
+        growth_rate -= 1
+
+    if leaves < 0:
+        leaves = 0
+    if growth_rate < 0:
+        growth_rate = 0
+    
+    return growth
+
+
     
 def is_yes_or_no(prompt="Please answer"):
     while True:
@@ -43,10 +81,27 @@ def is_yes_or_no(prompt="Please answer"):
 
 
 def assign_name():
+    global plantNames
+    global morpheus
+    global analies
+    global izzy
     if is_yes_or_no("Do you want to name the plant?"):
         plant_name = input("What do you want to name it? ")
     else:
-        plant_name = "Morpheus"
+        if morpheus == 0:
+            plant_name = plantNames[0]
+            morpheus += 1
+        elif analies == 0:
+            plant_name = plantNames[1]
+            analies += 1
+        elif izzy == 0:
+            plant_name = plantNames[2]
+            izzy += 1
+        else:
+            plant_name = plant_name[0]
+            morpheus = 1
+            analies = 0
+            izzy = 0
     
     print(f"Your plant's name is {plant_name}!")
 
@@ -74,11 +129,15 @@ def farewell_player():
     sys.exit()
 
 
-def apply_growth():
+def apply_growth(growth):
+    global growth_rate
     global height
     global leaves
-    height += 2
-    leaves += 2
+
+    if growth:
+        height += 1.5
+        gain_leaves = 2 + 2.5 * growth_rate
+        leaves += round(gain_leaves)
     print(f"Height: {height}")
     print(f"Leaves: {leaves}")
 
@@ -89,7 +148,7 @@ def game_loop():
     height = 0
     leaves = 0
 
-    while is_yes_or_no("contimue?"):
+    while is_yes_or_no("continue?"):
 
         if day <= 5:
             print(f"Day {day}")
@@ -98,9 +157,11 @@ def game_loop():
             day += 1
         elif height <= 32:
             print(f"Day {day}")
-            weather = choose_weathear()
-            print(weather)
-            apply_growth()
+            today_weather = choose_weather()
+            print(today_weather)
+            growth = weather_effects(today_weather)
+            
+            apply_growth(growth)
             messsage = store_message(day)
             print(messsage)
             day += 1
@@ -118,6 +179,8 @@ def main():
     greet_player()
 
     while is_yes_or_no("\nWould you like to grow a new plant?\n"):
+        assign_name()
+        print("In the Computer's digital world, time moves much faster than it does outside.")
         game_loop()
 
     farewell_player()
